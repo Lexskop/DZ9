e136576719764e8fe684141895b9bf3279f46bd3
lexskop <lexskop@gmail.com>
2022-10-15 18:06:57 +0700

    Решена вторая задачка

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,3 +3,22 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+Console.WriteLine("Введите  M");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  N");
+int end = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Ваши числа M = {start} и N = {end} -> ");
+int sum = 0;
+
+printRange(start, end);
+
+void printRange(int min, int max)
+{
+    if (min == max + 1)
+    {
+        Console.Write($"{sum}");
+        return;
+    }
+    sum = sum + min;
+    printRange(min + 1, max);
+}
